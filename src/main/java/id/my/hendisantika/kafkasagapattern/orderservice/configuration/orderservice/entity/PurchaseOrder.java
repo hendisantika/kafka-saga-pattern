77848b74b1599c7f4224b4f7c3be42eaaa092d39
hendisantika <hendisantika@yahoo.co.id>
2024-06-18 09:50:58 +0700

    Add PurchaseOrder Entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/kafkasagapattern/orderservice/configuration/orderservice/entity/PurchaseOrder.java
@@ -0,0 +1,32 @@
+package id.my.hendisantika.kafkasagapattern.orderservice.configuration.orderservice.entity;
+
+import id.my.hendisantika.kafkasagapattern.model.enums.OrderStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/06/24
+ * Time: 09.49
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Entity
+@ToString
+public class PurchaseOrder {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Integer userId;
+    private Integer productId;
+    private Integer price;
+    private OrderStatus status;
+}
