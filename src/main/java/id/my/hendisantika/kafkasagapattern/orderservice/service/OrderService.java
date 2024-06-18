f39a4ab7a3b40cccdfe8b8714e640b894b43ee28
hendisantika <hendisantika@yahoo.co.id>
2024-06-18 09:56:26 +0700

    Add OrderService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/kafkasagapattern/orderservice/service/OrderService.java
@@ -0,0 +1,36 @@
+package id.my.hendisantika.kafkasagapattern.orderservice.service;
+
+import id.my.hendisantika.kafkasagapattern.orderservice.eventhandlers.OrderEventPublisherService;
+import id.my.hendisantika.kafkasagapattern.orderservice.repository.PurchaseOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/06/24
+ * Time: 09.55
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class OrderService {
+
+    // product price map
+    private static final Map<Integer, Integer> PRODUCT_PRICE = Map.of(
+            1, 100,
+            2, 200,
+            3, 300
+    );
+
+    private final PurchaseOrderRepository purchaseOrderRepository;
+
+    private final OrderEventPublisherService eventPublisherService;
+
+}
