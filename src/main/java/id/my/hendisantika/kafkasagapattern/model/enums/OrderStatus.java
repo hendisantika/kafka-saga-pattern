d28a0b16a0f6f513c0a0635ea0726ef04b614775
hendisantika <hendisantika@yahoo.co.id>
2024-06-18 09:44:55 +0700

    Add Enum OrderStatus 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/kafkasagapattern/model/enums/OrderStatus.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.kafkasagapattern.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/06/24
+ * Time: 09.44
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OrderStatus {
+    ORDER_CREATED,
+    ORDER_CANCELLED,
+    ORDER_COMPLETED
+}
